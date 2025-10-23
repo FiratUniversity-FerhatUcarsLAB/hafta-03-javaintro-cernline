b6b4325aa96965ea7299ceb9075fe73af4a9c3de
Ceren Çam <cerencam001@gmail.com>
2025-10-23 16:34:02 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,11 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+        System.out.println ("Dikdörtgenin en: " + en + ", boy: " + boy);
+        System.out.println ("Alan = " + alan);
+        System.out.println ("Çevre = " + cevre);
+    }
+}
