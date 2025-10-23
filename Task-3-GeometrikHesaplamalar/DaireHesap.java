05d111b2c65a4c34e44f9a8d80bb15f52bfdccea
Ceren Çam <cerencam001@gmail.com>
2025-10-23 16:39:14 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,10 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double yaricap = 5.5;
+        double alan = Math.PI * yaricap * yaricap;
+        double cevre = 2 * Math.PI * yaricap;
+        System.out.println("Dairenin yarıçapı: " + yaricap);
+        System.out.println("Alan = πr² = " + alan);
+        System.out.println("Çevre = 2πr = " + cevre);
+    }
+}
