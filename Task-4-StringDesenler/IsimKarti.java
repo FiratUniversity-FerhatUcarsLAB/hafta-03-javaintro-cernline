003189c0b2d1da3d9f9420662407fb4109d7c1fb
Ceren Çam <cerencam001@gmail.com>
2025-10-23 17:03:18 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Ceren";
+        String soyad = "Çam";
+        String ogrNo = "250541073";
+        String bolum = "Yazılım Mühendisliği";
+        System.out.println("******************");
+        System.out.println("OGRENCI BILGILERI");
+        System.out.println("******************");
+        System.out.println("Ad: " + ad);
+        System.out.println("Soyad: " + soyad);
+        System.out.println("Öğrenci No: " + ogrNo);
+        System.out.println("Bölüm: " + bolum);
+    }
+}
