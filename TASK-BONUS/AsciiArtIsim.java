0d31225a9cd8b6b79786e3e17938292c660f560b
Ceren Çam <cerencam001@gmail.com>
2025-10-23 19:14:44 +0300

    Add AsciiArtIsim class to display ASCII art

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("  CCCCC   EEEEE  RRRR   EEEEE  N   N");
+        System.out.println(" C        E      R   R  E      NN  N");
+        System.out.println(" C        EEEE   RRRR   EEEE   N N N");
+        System.out.println(" C        E      R  R   E      N  NN");
+        System.out.println("  CCCCC   EEEEE  R   R  EEEEE  N   N");
+    }
+}
